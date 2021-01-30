3010654a8f546dc41301d3a48355a2405e112b37
ANMOL-SHAH <48213661+ANMOL-SHAH@users.noreply.github.com>
2021-01-31 00:37:46 +0530

    Delete 177- Nth Highest Salary.sql

--- 177- Nth Highest Salary.sql
+++
@@ -1,7 +0,0 @@
-CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
-BEGIN
-  RETURN (
-      # Write your MySQL query statement below.
-      SELECT Max(Derv) FROM (SELECT Salary AS Derv  From Employee ORDER BY Salary ASC LIMIT N) AS X
-  );
-END
